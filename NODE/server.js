812b82b5f008380bee560a8f5cd07ef67b3a1de8
ArielFrajacomo <arielfrajacomo@gmail.com>
2026-04-23 23:54:54 -0300

    Add JSON parsing and BurgerList endpoints
    Enable JSON body parsing and implement burger list API. server.js: add express.json() middleware, add a sample POST /example, replace the old /getBurgerList logic with an in-memory burgerList constant, add GET /BurgerList to return the list and POST /BurgerList to accept an array and update matching burgers by name (returns 202 on success, 400 for invalid format). Note: there's a stray incomplete "app." line near the end which will cause a syntax error and should be removed. Also added developer setup/run notes in comments. index.html: add HTTP 202 description and reorganize documentation sections (Server Side, request/response methods, Object Notation) for clarity.

--- NODE/server.js
+++ NODE/server.js
@@ -5,6 +5,7 @@ import express from 'express'; // new way of importing modules in Node.js, using
 
 
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
 const port = 3000;
 
 
@@ -12,34 +13,83 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-//req is the request object that contains information about the incoming request, such as query parameters, headers, and body data. 
-//res res is the response object that allows you to send a response back to the client.
 
-app.get('/getBurgerList', (req, res) => {
-    const list =  [
-        { "name": "X-Salada", "qtd": 0, "price": 30, "vegan": false, "src": "./assets/xsalada.jpeg"},
-        { "name": "X-Bacon", "qtd": 0, "price": 34, "vegan": false, "src": "./assets/xbacon.png" },
-        { "name": "X-Bacon Egg", "qtd": 0, "price": 39, "vegan": false, "src": "./assets/bacon-egg.png" },
-        { "name": "Monstruoso", "qtd": 0, "price": 50, "vegan": false, "src": "./assets/monstruoso.png" },
-        { "name": "Big Vegano", "qtd": 0, "price": 55, "vegan": true, "src": "./assets/xvegan.png" },
-        { "name": "X-Vegan", "qtd": 0, "price": 45, "vegan": true, "src": "./assets/monstruoso-vegan.png" },
-        { "name": "A Moda", "qtd": 0, "price": 75, "vegan": false, "src": "" }
-    ]
-    res.json(list); 
-    // sends the list of burgers as a JSON response to the client, 
-    // it can be accessed in the client-side JavaScript using fetch or XMLHttpRequest to retrieve the data and display it on the webpage.
-    // it can be seeing in the console of the browser when the client makes a request to the /getBurgerList endpoint, it will log the list of burgers in JSON format.
+//req is the request object that contains information about the incoming request, such as query parameters, headers, and body data. 
+//res is the response object that allows you to send a response back to the client.
+app.post('/example', (req, res) => {
+    res.json({ message: 'Item submitted successfully' });
 });
+
+
+//
+//    Burgerlist Area
+//
+
+//simulates a database of burgers, which can be accessed and modified through the defined routes.
+const burgerList = [
+    { "name": "X-Salada", "qtd": 0, "price": 30, "vegan": false, "src": "./assets/xsalada.jpeg"},
+    { "name": "X-Bacon", "qtd": 0, "price": 34, "vegan": false, "src": "./assets/xbacon.png" },
+    { "name": "X-Bacon Egg", "qtd": 0, "price": 39, "vegan": false, "src": "./assets/bacon-egg.png" },
+    { "name": "Monstruoso", "qtd": 0, "price": 50, "vegan": false, "src": "./assets/monstruoso.png" },
+    { "name": "Big Vegano", "qtd": 0, "price": 55, "vegan": true, "src": "./assets/xvegan.png" },
+    { "name": "X-Vegan", "qtd": 0, "price": 45, "vegan": true, "src": "./assets/monstruoso-vegan.png" },
+    { "name": "A Moda", "qtd": 0, "price": 75, "vegan": false, "src": "" }
+]
+
+// sends the list of burgers as a JSON response to the client, 
+// used Thunderclient to test the endpoint and received the list of burgers in the response body.
+app.get('/BurgerList', (req, res) => {
+    res.json(burgerList); 
+});
+
+app.post('/BurgerList', (req, res) => {
+  // Expecting an array of burgers in req.body
+  if (Array.isArray(req.body)) {
+    // Update only burgers that match by 'name'
+    req.body.forEach(incomingBurger => {
+      const existing = burgerList.find(b => b.name === incomingBurger.name);
+      if (existing) {
+        // Update only the fields present in the incoming burger
+        Object.keys(incomingBurger).forEach(key => {
+          if (key !== 'name') {
+            existing[key] = incomingBurger[key];
+          }
+        });
+      }
+    });
+    res.status(202).json(burgerList); // Return the updated burger list with a 202 Accepted status.
+  } else {
+    res.status(400).json({ message: 'Invalid data format. Expected an array.' });
+  }
+});
+
+app.
+
+
 
 
 
 
 //app.listen(port) // starts the server and listens on the specified port 
+//must be called after defining all the routes and middleware to ensure that the server is properly set up before it starts accepting requests.
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
 
+/* DONT FORGET
 
+installing express, you need to run the following commands in your terminal:
+  npm init -y
+  npm install express
 
+running the server, you can use the following command in your terminal:
+  node server.js
 
+!!but don't forget to go to the folder.
+!!in this case, you need to run the following command:
+    
+    cd NODE 
+    node server.js
+
+*/
